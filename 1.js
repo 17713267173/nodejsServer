da4692fb2d3e0014386405e185b0b0bfeb7aa4a3
chenjinbao <845325935@qq.com>
2020-08-05 23:25:18 +0800

    练习

--- 1.js
+++ 1.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/html;charset="UTF-8"'});
-	res.write('你好');
+	var data = fs.readFileSync('index.html','utf-8');
+	res.write(data);
 	res.end();
 }).listen(1000);
