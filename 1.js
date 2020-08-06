5f8cc23bd9cf4f84123097d55afea2928fe84b7c
chenjinbao <845325935@qq.com>
2020-08-06 13:52:14 +0800

    练习

--- 1.js
+++ 1.js
@@ -2,19 +2,28 @@ var http = require('http');
 var fs = require('fs');
 http.createServer(function(req,res){
 	if(req.url != '/favicon.ico'){
-		res.writeHead(200,{'Content-Type':'text/html;charset="UTF-8"'});
-		switch(req.url){
-			case '/':
-				var data = fs.readFileSync('index.html','utf-8');
-				break;
-			case '/login':
-				var data = fs.readFileSync('login.html','utf-8');
-				break;
-			default:
-				var data = fs.readFileSync('index.html','utf-8');
-				break;
+		console.log(req.url)
+		if(req.url == '/img'){
+			res.writeHead(200,{'Content-Type':'image/jpeg;'});
+			fs.readFile('./images/0701.jpg','binary',function(err,data){
+				res.write(data,'binary');
+				res.end();
+			});
+		}else{
+			res.writeHead(200,{'Content-Type':'text/html;charset="UTF-8"'});
+			switch(req.url){
+				case '/':
+					var data = fs.readFileSync('./view/index.html','utf-8');
+					break;
+				case '/login':
+					var data = fs.readFileSync('./view/login.html','utf-8');
+					break;
+				default:
+					var data = fs.readFileSync('./view/404.html','utf-8');
+					break;
+			}
+			res.write(data);
+			res.end();
 		}
-		res.write(data);
-		res.end();
 	}
 }).listen(1000);
