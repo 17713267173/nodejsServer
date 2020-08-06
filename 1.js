3f705f3fdb22cd37d5955bc22166509c49bc1e32
chenjinbao <845325935@qq.com>
2020-08-06 11:06:23 +0800

    练习

--- 1.js
+++ 1.js
@@ -1,8 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/html;charset="UTF-8"'});
-	var data = fs.readFileSync('index.html','utf-8');
-	res.write(data);
-	res.end();
+	if(req.url != '/favicon.ico'){
+		res.writeHead(200,{'Content-Type':'text/html;charset="UTF-8"'});
+		switch(req.url){
+			case '/':
+				var data = fs.readFileSync('index.html','utf-8');
+				break;
+			case '/login':
+				var data = fs.readFileSync('login.html','utf-8');
+				break;
+			default:
+				var data = fs.readFileSync('index.html','utf-8');
+				break;
+		}
+		res.write(data);
+		res.end();
+	}
 }).listen(1000);
