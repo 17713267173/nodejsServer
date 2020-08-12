ab47d7f76a5c1e28115870be94d521f15cd0d134
chenjinbao <845325935@qq.com>
2020-08-12 13:35:01 +0800

    练习

--- 1.js
+++ 1.js
@@ -1,29 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var router = require('./router');
 http.createServer(function(req,res){
-	if(req.url != '/favicon.ico'){
-		console.log(req.url)
-		if(req.url == '/img'){
-			res.writeHead(200,{'Content-Type':'image/jpeg;'});
-			fs.readFile('./images/0701.jpg','binary',function(err,data){
-				res.write(data,'binary');
-				res.end();
-			});
+	if(req.url !== '/favicon.ico'){
+		if(req.url.indexOf('images') != -1){
+			router.imgRouter(req,res);
 		}else{
-			res.writeHead(200,{'Content-Type':'text/html;charset="UTF-8"'});
-			switch(req.url){
-				case '/':
-					var data = fs.readFileSync('./view/index.html','utf-8');
-					break;
-				case '/login':
-					var data = fs.readFileSync('./view/login.html','utf-8');
-					break;
-				default:
-					var data = fs.readFileSync('./view/404.html','utf-8');
-					break;
-			}
-			res.write(data);
-			res.end();
+			router.htmlRouter(req,res);
 		}
 	}
 }).listen(1000);
